26721902bd2dd81e13549a86ed5d374e20ec436c
Youngwon Choi <88872326+zeroone-universe@users.noreply.github.com>
2022-01-17 10:32:46 +0900

    Time: 44 ms (24.09%), Space: 14.2 MB (80.26%) - LeetHub

--- 290-word-pattern/290-word-pattern.py
+++ 290-word-pattern/290-word-pattern.py
@@ -2,8 +2,12 @@ class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
         s=s.split()
         wpdict=dict()
+        
+        #예외 처리, s와 pattern의 길이 다르면 안됨.
         if len(s)!=len(pattern):
             return False
+        
+        
         for i in range(len(pattern)):
             if (pattern[i], s[i]) in wpdict.items():
                 continue
