1701a3810617feacb14c2b7dee3cc5d388499195
Youngwon Neo Choi <88872326+zeroone-universe@users.noreply.github.com>
2022-04-09 18:13:58 +0900

    Time: 44 ms (51.10%), Space: 13.9 MB (82.27%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
@@ -1,29 +1,30 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        if not digits:
-            return []
+        dic = {"2": "abc",
+              "3": "def",
+              "4": "ghi",
+              "5": "jkl",
+              "6": "mno",
+              "7": "pqrs",
+              "8": "tuv",
+              "9": "wxyz"}
+        result = []
         
-        def dfs(index, comb):
-            if len(digits)==len(comb):
-                result.append(comb)
+        if not digits:
+            return result
+        
+        def dfs(index, word):
+            if len(word) == len(digits):
+                result.append(word)
                 return
-            
             for i in range(index, len(digits)):
                 for j in dic[digits[i]]:
-                    dfs(i+1, comb+j)
+                    dfs(i+1, word + j)
         
-        dic={
-            '2':['a','b','c'],
-            '3':['d','e','f'],
-            '4':['g','h','i'],
-            '5':['j','k','l'],
-            '6':['m','n','o'],
-            '7':['p','q','r','s'],
-            '8':['t','u','v'],
-            '9':['w','x','y','z']
-        }
-
-        result=[]
-        dfs(0,'')
+        
+        
+        dfs(0, "")
         
         return result
+        
+            
