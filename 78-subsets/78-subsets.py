57228a233d4c38d84ca3824aecd4b703f7693a92
Youngwon Neo Choi <88872326+zeroone-universe@users.noreply.github.com>
2022-04-10 23:03:12 +0900

    Time: 62 ms (19.45%), Space: 14 MB (80.57%) - LeetHub

--- 78-subsets/78-subsets.py
+++ 78-subsets/78-subsets.py
@@ -1,17 +1,19 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         result = []
+        nums.sort()
         
-        def dfs(subset):
+        def dfs(subset, index):
             result.append(subset[:])
-            if subset[-1] == nums[-1]:
+            if index == len(nums)-1:
                 return
-            for idx in range(nums.index(subset[-1])+1, len(nums)):
-                dfs(subset + [nums[idx]])
-        
-        for num in nums:
-            dfs([num])
             
-        result.append([])
+            for i in range(index+1, len(nums)):
+                dfs(subset + [nums[i]], i)
+            
+        dfs([], -1)
         return result
+            
+            
+        
         
