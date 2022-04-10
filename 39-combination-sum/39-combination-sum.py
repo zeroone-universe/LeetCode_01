a5b5f12fb3023e06295ab4a35ae5b3d1bcde4a07
Youngwon Neo Choi <88872326+zeroone-universe@users.noreply.github.com>
2022-04-10 22:56:25 +0900

    Time: 132 ms (36.46%), Space: 14 MB (61.34%) - LeetHub

--- 39-combination-sum/39-combination-sum.py
+++ 39-combination-sum/39-combination-sum.py
@@ -1,7 +1,7 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         result = []
-        sorted_cand = sorted(candidates)
+        candidates.sort()
         
         def dfs(nums, index):
             if sum(nums) == target:
@@ -10,8 +10,8 @@ class Solution:
             elif sum(nums) > target:
                 return
             
-            for i in range(index, len(sorted_cand)):
-                dfs(nums+[sorted_cand[i]],i)
+            for i in range(index, len(candidates)):
+                dfs(nums+[candidates[i]],i)
                 
         dfs([], 0)
         return result
