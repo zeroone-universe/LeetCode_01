5262b93cfe768695a94cc363d64ec0b5349648bf
Youngwon Neo Choi <88872326+zeroone-universe@users.noreply.github.com>
2022-04-10 22:39:38 +0900

    Time: 90 ms (95.91%), Space: 15.9 MB (53.18%) - LeetHub

--- 77-combinations/77-combinations.py
+++ 77-combinations/77-combinations.py
@@ -1,20 +1,8 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
         
-        result = []
+        
         tot = [i+1 for i in range(0, n)]
         
-        def dfs(comb, max_num):
-            if len(comb) == k :
-                result.append(comb[:])
-                return
-            
-            for j in tot:
-                if j >max_num:
-                    new_comb = comb+[j]
-                    dfs(new_comb, max(new_comb))
-                    
-        dfs([],0)
-        return result
-            
-            
+        
+        return list(itertools.combinations(tot, k ))
