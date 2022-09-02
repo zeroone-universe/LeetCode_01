d1ad2245facabf467cb472a477452115b1f35799
Youngwon Neo Choi <88872326+zeroone-universe@users.noreply.github.com>
2022-09-02 14:48:11 +0900

    Time: 42 ms (75.08%), Space: 13.8 MB (95.58%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.py
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -1,14 +1,16 @@
 class Solution:
+    
+    
     def fib(self, n: int) -> int:        
-        dp = list()
-        dp.append(0)
-        dp.append(1)
+        dp = collections.defaultdict(int)
+        
+        dp[0] = 0
+        dp[1] = 1
         
         if n<2:
             return dp[n]
         
-        for idx in range(2, n):
-            dp.append(dp[idx-1]+dp[idx-2])
+        for i in range(2, n+1):
+            dp[i] = dp[i-1]+dp[i-2]
             
-        return dp[n-1] + dp[n-2]
-            
+        return dp[n]
