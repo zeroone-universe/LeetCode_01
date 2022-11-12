0ccc3dbd9fe4d41bcef0c6380da8a89fd0061de6
Youngwon Neo Choi <88872326+zeroone-universe@users.noreply.github.com>
2022-11-12 15:51:16 +0900

    Time: 80 ms (49.50%), Space: 14.2 MB (24.09%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -8,7 +8,9 @@ class Solution:
             
             for ele in nums:
                 if ele not in discovered:
-                    dfs(discovered + [ele])
+                    new_dis = discovered[:]
+                    new_dis.append(ele)
+                    dfs(new_dis)
                     
         dfs([])
         
