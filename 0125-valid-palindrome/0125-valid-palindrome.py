1efe1008a8441ed0b24c44e6c68af12b7268793b
Youngwon Neo Choi <88872326+zeroone-universe@users.noreply.github.com>
2023-03-28 02:43:12 +0900

    Time: 209 ms (29.94%), Space: 15.6 MB (29.91%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        
+        strs = []
+        for char in s:
+            if char.isalnum():
+                strs.append(char.lower())
+        
+        while len(strs) > 1:
+            if strs.pop(0) != strs.pop():
+                return False
+            
+        return True
+            
