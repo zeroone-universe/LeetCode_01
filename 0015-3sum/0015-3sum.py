d44309513dd6cbcaef247f2fcbfd1aed7c0bded2
Youngwon Neo Choi <88872326+zeroone-universe@users.noreply.github.com>
2023-03-31 00:33:05 +0900

    Time: 5048 ms (13.81%), Space: 16.7 MB (93.55%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -7,7 +7,10 @@ class Solution(object):
         nums.sort()
         result = []
         
-        for i in range(len(nums)):
+        for i in range(len(nums)-2):
+            if i>0 and nums[i]==nums[i-1]:
+                continue
+                
             left = i+1
             right = len(nums)-1
             
@@ -17,14 +20,18 @@ class Solution(object):
                         result.append([nums[i], nums[left], nums[right]]) 
                     left+=1
                     right-=1
-        
+                    while left<right and nums[left] == nums[left-1]:
+                        left+=1
+                    while left < right and nums[right] == nums[right+1]:
+                        right-=1
 
                 elif nums[left]+ nums[right] < -nums[i]:
                     left+=1
-                 
+                
+                        
                 elif nums[left]+ nums[right] > -nums[i]:
                     right-=1
-          
+                 
         return result
             
                
