306b7236704893bcaf519391092c417fae746b0e
Youngwon Neo Choi <88872326+zeroone-universe@users.noreply.github.com>
2023-04-12 16:12:24 +0900

    Time: 26 ms (52.41%), Space: 13.4 MB (92.41%) - LeetHub

---
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def removeDuplicateLetters(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        
+        counter = collections.Counter(s)
+        stack = []
+        seen = set()
+        
+        for char in s:
+            counter[char] -= 1
+            if char in seen:
+                continue
+                
+            while stack and char < stack[-1] and counter[stack[-1]] > 0:
+                seen.remove(stack.pop())
+                
+            stack.append(char)
+            seen.add(char)
+        
+        return ''.join(stack)
