ee37e990338332d8706eea54a88a4a0be8b0088f
Youngwon Neo Choi <88872326+zeroone-universe@users.noreply.github.com>
2023-04-12 16:29:52 +0900

    Time: 1142 ms (71.82%), Space: 27.4 MB (56.99%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,13 +1,16 @@
-class Solution:
-    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        output = [0] * len(temperatures)
+class Solution(object):
+    def dailyTemperatures(self, temperatures):
+        """
+        :type temperatures: List[int]
+        :rtype: List[int]
+        """
         stack = []
-
-        for idx in range(len(temperatures)):
-            while stack and stack[-1][1] < temperatures[idx]:
-                ele = stack.pop()
-                output[ele[0]] = idx-ele[0]
-
-            stack.append([idx, temperatures[idx]])
+        output = [0]*len(temperatures)
+        
+        for idx, temp in enumerate(temperatures):
+            while stack and stack[-1][1] < temp:
+                date, _ = stack.pop()
+                output[date] = (idx - date)
+            stack.append((idx, temp))
 
         return output
