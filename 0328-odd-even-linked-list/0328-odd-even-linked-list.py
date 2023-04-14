ad3fd197459d00bcfe40fef8e88840247cb4c5ff
Youngwon Neo Choi <88872326+zeroone-universe@users.noreply.github.com>
2023-04-15 01:50:02 +0900

    Time: 25 ms (87.98%), Space: 17 MB (60.39%) - LeetHub

---
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.py
@@ -0,0 +1,31 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def oddEvenList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if head is None:
+            return head
+        
+        root = head
+        odd = head
+        even = even_save = head.next
+        # even_save = head.next
+        
+        while even and even.next:
+            odd.next = odd.next.next
+            odd = odd.next
+            
+            even.next= even.next.next
+            even = even.next
+                
+            
+        odd.next = even_save
+        
+        return root
+        
