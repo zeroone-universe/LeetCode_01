c84de2a286437002bda96fbba641500ba33c5d16
Youngwon Neo Choi <88872326+zeroone-universe@users.noreply.github.com>
2023-04-25 15:36:25 +0900

    Time: 36 ms (88.59%), Space: 13.8 MB (53.13%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -4,14 +4,29 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        max_len = 0
         
-        for idx in range(len(s)):
-            length = 1
-            while len(s) > idx+length and s[idx+length] not in s[idx:idx+length]:
-                length += 1
+#         max_len = 0
+#         start = 0
+#         used = {}
+        
+#         for idx, char in enumerate(s):
+#             if char in used and start <= used[char]:
+#                 start = used[char]+1
                 
-            max_len = max(length, max_len)
+#                 max_len = max(idx-start+1, max_len) 
+#             used[char] = idx
+#         return max_len
             
-        return max_len
-            
+        used = {}
+        max_length = start = 0
+        for index, char in enumerate(s):
+            # 이미 등장했던 문자라면 `start` 위치 갱신
+            if char in used and start <= used[char]:
+                start = used[char] + 1
+            else:  # 최대 부분 문자열 길이 갱신
+                max_length = max(max_length, index - start + 1)
+
+            # 현재 문자의 위치 삽입
+            used[char] = index
+
+        return max_length
