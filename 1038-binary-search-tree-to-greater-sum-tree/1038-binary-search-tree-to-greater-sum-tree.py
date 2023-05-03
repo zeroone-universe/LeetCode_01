14ac6b41942a4f1a3122c16a1a064bac93dc5f8d
Youngwon Neo Choi <88872326+zeroone-universe@users.noreply.github.com>
2023-05-03 11:06:52 +0900

    Time: 23 ms (32.86%), Space: 13.4 MB (61.43%) - LeetHub

--- 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.py
+++ 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.py
@@ -1,17 +1,24 @@
 # Definition for a binary tree node.
-# class TreeNode:
+# class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    val = 0
-    
-    def bstToGst(self, root: TreeNode) -> TreeNode:
-        if root:
-            self.bstToGst(root.right)
-            self.val += root.val
-            root.val = self.val
-            self.bstToGst(root.left)
+class Solution(object):
+    result = 0
+    def bstToGst(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        def LNR(node):
+            if not node:
+                return
+            LNR(node.right)
+            self.result += node.val
+            node.val = self.result
+            LNR(node.left)
             
+        LNR(root)
+        
         return root
