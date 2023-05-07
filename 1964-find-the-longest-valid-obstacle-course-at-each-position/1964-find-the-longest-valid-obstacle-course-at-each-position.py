14a6978d46d134864c3f019bf817cbf2ceee4fe5
Youngwon Neo Choi <88872326+zeroone-universe@users.noreply.github.com>
2023-05-08 00:39:25 +0900

    Time: 1501 ms (33.33%), Space: 32.2 MB (100.00%) - LeetHub

---
+++ 1964-find-the-longest-valid-obstacle-course-at-each-position/1964-find-the-longest-valid-obstacle-course-at-each-position.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def longestObstacleCourseAtEachPosition(self, obstacles):
+        """
+        :type obstacles: List[int]
+        :rtype: List[int]
+        """
+        n = len(obstacles)
+        answer = [1] * n
+        
+        # lis[i] records the lowest increasing sequence of length i + 1.
+        lis = []
+    
+        for i, height in enumerate(obstacles):
+            # Find the rightmost insertion position idx.
+            idx = bisect.bisect_right(lis, height)
+            
+            if idx == len(lis):
+                lis.append(height)
+            else:
+                lis[idx] = height
+            answer[i] = idx + 1
+            
+        return answer
